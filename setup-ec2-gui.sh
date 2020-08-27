5f176c4df94a2a1a871438325628a31555ea3d8a
gy741 <gy741.kim@gmail.com>
2020-08-27 13:01:11 +0900

    Update setup-ec2-gui.sh

--- setup-ec2-gui.sh
+++ setup-ec2-gui.sh
@@ -2,6 +2,7 @@
 
 #echo 'ubuntu:korea123!!' | chpasswd
 sudo -S sh -c 'echo "ubuntu:korea123!!" | chpasswd'
+sudo usermod -aG docker ubuntu
 
 echo "deb https://packagecloud.io/firstlookmedia/code/ubuntu/ bionic main" | sudo tee -a /etc/apt/sources.list.d/firstlookmedia_code.list
 curl -L https://packagecloud.io/firstlookmedia/code/gpgkey | sudo apt-key add -
