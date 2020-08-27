933bab47be5ad7d4c494912a22fafaa8d21284e1
gy741 <gy741.kim@gmail.com>
2020-08-27 10:40:05 +0900

    Create setup-ec2-gui.sh

---
+++ setup-ec2-gui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'korea123!!' | passwd --stdin ubuntu
+
+sudo apt-get update
+sudo apt-get install -y ubuntu-desktop xrdp curl gnupg apt-transport-https
+curl -L https://packagecloud.io/firstlookmedia/code/gpgkey | sudo apt-key add -
+echo "deb https://packagecloud.io/firstlookmedia/code/ubuntu/ bionic main" | sudo tee -a /etc/apt/sources.list.d/firstlookmedia_code.list
+
+sudo apt-get update
+sudo apt install -y dangerzone
