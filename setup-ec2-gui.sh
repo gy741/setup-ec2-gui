4401453dcc62134dddcf5486639d08eb5c274988
gy741 <gy741.kim@gmail.com>
2020-08-27 12:43:43 +0900

    Update setup-ec2-gui.sh

--- setup-ec2-gui.sh
+++ setup-ec2-gui.sh
@@ -3,10 +3,9 @@
 #echo 'ubuntu:korea123!!' | chpasswd
 sudo -S sh -c 'echo "ubuntu:korea123!!" | chpasswd'
 
-sudo apt-get update
-sudo apt-get install -y ubuntu-desktop xrdp curl gnupg apt-transport-https
-curl -L https://packagecloud.io/firstlookmedia/code/gpgkey | sudo apt-key add -
 echo "deb https://packagecloud.io/firstlookmedia/code/ubuntu/ bionic main" | sudo tee -a /etc/apt/sources.list.d/firstlookmedia_code.list
+curl -L https://packagecloud.io/firstlookmedia/code/gpgkey | sudo apt-key add -
 
 sudo apt-get update
-sudo apt install -y dangerzone
+sudo apt-get install -y ubuntu-desktop xrdp curl gnupg apt-transport-https dangerzone
+
